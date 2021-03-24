7ee297ccf12e7bad8b8e0f040799e8e647959fd1
Chris Parnin <chris.parnin@gmail.com>
2021-03-24 13:43:30 -0400

    remove feature flag.

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ const redis = require('redis');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  let client = redis.createClient(6379, '127.0.0.1', {});
-  client.get('RECENT', async function(err, value)
-  {
-    res.render('index', { recentFlag: getFlag(value), title: 'meow.io', recentUploads: await db.recentCats(5), bestFacts: (await db.votes()).slice(0,100) });
-    client.quit();
-  });
+  //let client = redis.createClient(6379, '127.0.0.1', {});
+  res.render('index', 
+    { recentFlag: getFlag('ON'), 
+      title: 'meow.io', 
+      recentUploads: await db.recentCats(5), 
+      bestFacts: (await db.votes()).slice(0,100) 
+    });
+  // client.quit();
 });
 
 function getFlag(value)
